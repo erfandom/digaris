f93a95912c6c62bdd50e775302fcdd9e694ac0f3
kohryan <rw.januardi@gmail.com>
2022-03-24 00:51:52 +0800

    Router

--- pages/login/humas.js
+++ pages/login/humas.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from 'next/router'
+
 
 // layout for page
 
 import Footer from "/components/Footers/Footer.js";
 
-export default function Login() {
+export default function Login({href}) {
+  const router = useRouter();
+  const [password, setPassword] = useState("");
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    if (password == "humas74") {
+      router.push("https://drive.google.com/drive/folders/1AdzdLrB7mudY-Vg0_RuZb_yX6PHG5iqa?usp=sharing")
+    }
+  };
+
   return (
     <>
     <div
@@ -21,7 +33,7 @@ export default function Login() {
               <img
                 alt="..."
                 className="shadow-md rounded-full max-w-full w-16 mx-auto p-2 bg-white"
-                src="/img/logo/sp.png"
+                src="/img/logo/humas.png"
               />
                 <div className="text-center mb-3 mt-4">
                   <h6 className="text-blueGray-500 text-sm font-bold">
@@ -30,7 +42,7 @@ export default function Login() {
                 </div>
               </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                <form>
+                <form onClick={submitForm}>
                   <div className="relative w-full mb-3">
                     <label
                       className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
@@ -41,17 +53,15 @@ export default function Login() {
                     <input
                       type="password"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Kata Kunci"
+                      placeholder="Kata Kunci" value={password} onChange={e => setPassword(e.target.value)}
                     />
                   </div>
 
                   <div className="text-center mt-6">
-                    <button
+                    <input
                       className="bg-indigo-600 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="button"
-                    >
-                      Masuk
-                    </button>
+                      type="submit" value="Masuk"
+                    />
                   </div>
                 </form>
               </div>
