86aa1a49a94ac25eb4e083e04660a1234ee4bdab
kohryan <rw.januardi@gmail.com>
2022-03-23 14:20:40 +0800

    Update humas.js

--- pages/login/humas.js
+++ pages/login/humas.js
@@ -16,7 +16,7 @@ export default function Login() {
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-4/12 px-4">
             <h6 className="text-white text-sm mb-6 mx-auto items-center w-full font-bold">
-            <a href="/"><i className="fas fa-chevron-circle-left"></i>  Kembali ke Beranda </a>
+            <Link href="/"><i className="fas fa-chevron-circle-left"></i>  Kembali ke Beranda </Link>
             </h6>
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
               <div className="rounded-t mb-0 px-6 py-6">
