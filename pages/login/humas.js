34d66b8f6a9424f62ce2a8597c397c1b0dfac13a
kohryan <rw.januardi@gmail.com>
2022-03-23 22:53:54 +0800

    login

--- pages/login/humas.js
+++ pages/login/humas.js
@@ -1,9 +1,7 @@
 import React from "react";
-import Link from "next/link";
 
 // layout for page
 
-import Auth from "/components/layouts/Auth.js";
 import Footer from "/components/Footers/Footer.js";
 
 export default function Login() {
@@ -16,7 +14,7 @@ export default function Login() {
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-4/12 px-4">
             <h6 className="text-white text-sm mb-6 mx-auto items-center w-full font-bold">
-            <Link href="/"><i className="fas fa-chevron-circle-left"></i>  Kembali ke Beranda </Link>
+            <a href="http://localhost:3000"><i className="fas fa-chevron-circle-left"></i>  Kembali ke Beranda </a>
             </h6>
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
               <div className="rounded-t mb-0 px-6 py-6">
@@ -48,14 +46,12 @@ export default function Login() {
                   </div>
 
                   <div className="text-center mt-6">
-                  <a href="https://google.com">
                     <button
                       className="bg-indigo-600 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                       type="button"
                     >
                       Masuk
                     </button>
-                  </a>
                   </div>
                 </form>
               </div>
@@ -68,5 +64,3 @@ export default function Login() {
     </>
   );
 }
-
-Login.layout = Auth;
